5d8a4a4afe2579624cc24140357575af551c816d
Deven <deven.sharma@siemens.com>
2021-07-11 19:15:50 +0530

    add: aboutme page

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import React from 'react';
 import Header from './Components/Header/Header';
 import Footer from './Components/Footer/Footer';
 import Navbar from './Components/NavBar/Navbar'
+import Contact from './Components/Contact/Contact';
 
 function App() {
   return (
     <React.Fragment>
-      <Navbar/>
-      <Header/>
+      {/* <Navbar/>
+      <Header/> */}
+
+      <Contact></Contact>
     </React.Fragment>
   );
 }
---
+++ src/Components/Contact/Contact.js
@@ -0,0 +1,48 @@
+import classes from "./Contact.module.css";
+import email from '../../images/email.png'
+const Contact = ()=>{
+
+    return (<>
+                <div className={classes.container}>
+                    <div className={classes.header}>
+                        <h1>
+                           Let's Connect together. 
+                        </h1>
+                        <p>
+                            Please, provide your email and subscribe to my news letter,
+                            <div>we can also exchange messages via mail.
+                            </div>
+                        </p>
+                    </div>
+
+                    <div className={classes.form}>
+                        <form>
+                            <input  className = {classes.input} type="text" />
+                            {/* <input value="message" className = {classes.input} type="text" /> */}
+                            <button className={classes.btn}>
+                                Subscribe
+                            </button>
+ 
+                        </form>
+
+                    </div>
+                    <div className={classes.footer}>
+                        <h2>
+                            Let's Connect via Social Networking!
+                        </h2>
+                    </div>
+                    <div className={classes.social}>
+                     <button className={[classes.btn, classes.insta].join(' ')}>
+                         Instagram
+                     </button>
+                     <button className={[classes.btn, classes.linkedin].join(' ')}>
+                         Linkedin
+                     </button>
+                    </div>
+                    
+                </div>
+            </>)
+
+}
+
+export default Contact;
